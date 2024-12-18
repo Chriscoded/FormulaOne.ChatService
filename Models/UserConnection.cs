4a53a2c866926cc712141a56ff7f3d1a909eb9f3
chriscoded <amaemechris@gmail.com>
2024-12-18 17:10:11 +0100

    set up signalR hub

--- Models/UserConnection.cs
+++ Models/UserConnection.cs
@@ -2,5 +2,7 @@
 {
     public class UserConnection
     {
+        public string Username { get; set; } = string.Empty;
+        public string ChatRoom { get; set; } = string.Empty;
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
+using FormulaOne.ChatService.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSignalR();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -22,4 +25,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.MapHub<ChatHub>("/Chat");
 app.Run();
